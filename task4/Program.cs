cacc62b2b286014cebc5dce1c48515e5d6836316
mike_karni <3923499@mail.ru>
2022-04-15 01:07:42 +0300

    zada4a4_done

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+    Console.WriteLine( "ЭТОТ ДЕНЬ- ВЫХОДНОЙ!" );
+}
+/*if ( a > 7 || a<1)
+{
+    Console.WriteLine("Введите число от 1 до 7 ");
+}*/
+else
+{
+    Console.WriteLine("Это рабочий день");
+}
+
+/*int[] array = {1,2,3,4,5,6,7};
+Console.WriteLine("Введите день номер дня недели");
+Console.ReadLine();*/
