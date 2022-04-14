1986e863bfe62cd17acffff0c2f66b9530e2d147
mike_karni <3923499@mail.ru>
2022-04-15 00:43:28 +0300

    task3_done

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = RandomInt(10,1001);
+Console.WriteLine(number);
+string stringNumber = Convert.ToString(number);
+if (number%100 == number)
+{
+    Console.WriteLine("ТРЕТЬЕЙ ЦИФРЫ НЕТ!!!");
+}
+Console.WriteLine($"{stringNumber[2]}");
+
+int RandomInt(int enclusiveMin,int exclusiveMax)
+{
+    return new Random().Next(enclusiveMin,exclusiveMax);
+}
