904638d124ba2949c74e50fc4a5f4e310ec5923c
mike_karni <3923499@mail.ru>
2022-04-15 00:22:47 +0300

    zada4a1_done

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = RandomInt(100,1000);
+Console.WriteLine(number);
+string stringNumber = Convert.ToString(number);
+Console.WriteLine($"{stringNumber[1]}");
+
+int RandomInt(int enclusiveMin,int exclusiveMax)
+{
+    return new Random().Next(enclusiveMin,exclusiveMax);
+}
