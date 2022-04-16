2e1ba5024323924c4c07c6e3bbfaeaf30d77df18
mike_karni <3923499@mail.ru>
2022-04-16 19:13:35 +0300

    Добавил условие 2 задачи

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,9 @@
-﻿int number = RandomInt(100,1000);
+﻿ /*Напишите программу, которая выводит 
+случайное трёхзначное число и удаляет вторую цифру 
+этого числа.*/
+
+
+int number = RandomInt(100,1000);
 Console.WriteLine(number);
 string stringNumber = Convert.ToString(number);
 Console.WriteLine($"{stringNumber[0]}{stringNumber[2]}");
